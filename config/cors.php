b813f8184942d52dcc2722fdad792ecf8e216141
Antoine <136310380+antoine-codeur@users.noreply.github.com>
2024-09-05 12:02:44 +0200

    add http://localhost:5173 to Cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'), // Assuming this is in production or another environment
+        'http://localhost:5173' // Add this for your current development environment
+    ],
 
     'allowed_origins_patterns' => [],
 
